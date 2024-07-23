92dc09170e1fcfbb7262bc176a6b8b3bae862ab0
himeji. <88136704+h1meji@users.noreply.github.com>
2024-07-23 16:59:42 +0200

    IC WIP | dropping items

--- ToadClient/src/Toad/Modules/inventory_cleaner.cpp
+++ ToadClient/src/Toad/Modules/inventory_cleaner.cpp
@@ -24,6 +24,176 @@ namespace toadll
 			SLEEP(100);
 			return;
 		}
+
+		static bool cleaning = false;
+		static int index = -1;
+		static int step_counter = 0;
+		static bool stopped = false;
+		static bool dropped = false;
+
+		if (stopped)
+		{
+			if (m_isCtrlDown)
+			{
+				send_key(VK_CONTROL, false);
+				m_isCtrlDown = false;
+			}
+
+			if (!GetAsyncKeyState(inventory_cleaner::key))
+				stopped = false;
+
+			SLEEP(100);
+			return;
+		}
+
+		static Timer aim_timer;
+		static POINT current_pos{};
+
+		if ((GetAsyncKeyState(inventory_cleaner::key) & 0x8000 && CVarsUpdater::IsInGui) || cleaning)
+		{
+			UpdateSlotPosOffsets();
+
+			if (!cleaning)
+			{
+				{
+					std::lock_guard lock(m_mutex);
+
+					std::shared_ptr<c_Entity> localPlayer = MC->getLocalPlayer();
+					m_inventoryContents = localPlayer->getInventory();
+
+					if (m_inventoryContents.empty())
+					{
+						LOGDEBUG("[inventory cleaner] empty inventory");
+						SLEEP(100);
+						return;
+					}
+
+				}
+
+				// setup mouse path
+				SetupDropPath();
+				LOGDEBUG("[inventory cleaner] dropping {} items", m_indexDropPath.size() - 2);
+
+				LOGDEBUG("inventory size: {}", m_inventoryContents.size());
+				LOGDEBUG("drop path size: {}", m_indexDropPath.size());
+
+				step_counter = 0;
+
+				// get the first index from the path
+				index = m_indexDropPath.front();
+				m_indexDropPath.pop_front();
+				step_counter++;
+
+				// start holding shift and wait a bit
+				send_key(VK_CONTROL);
+				m_isCtrlDown = true;
+				SLEEP(rand_int(30, 50));
+
+				// start timer and set starting mouse position
+				aim_timer.Start();
+				GetCursorPos(&current_pos);
+			}
+		
+			if (!dropped && index != -1)
+			{
+				if (step_counter > 1 && ((GetAsyncKeyState(inventory_cleaner::key) & 0x8000) || GetAsyncKeyState(0x45 /*E*/) & 0x8000 || GetAsyncKeyState(VK_ESCAPE) & 0x8000))
+				{
+					step_counter = 0;
+					index = -1;
+					cleaning = false;
+					dropped = false;
+
+					stopped = true;
+
+					return;
+				}
+
+				const std::string& name = m_inventoryContents[index];
+				POINT pos = m_slotPosOffset[index];
+				const POINT middle = get_middle_of_screen();
+
+				pos.x += middle.x;
+				pos.y += middle.y;
+
+				if (aim_timer.Elapsed() > inventory_cleaner::delay)
+				{
+					// make sure mouse is on slot and right click
+					SetCursorPos(pos.x, pos.y);
+					SLEEP(rand_int(50, 60));
+
+					/*right_mouse_down(pos);
+					SLEEP(rand_int(35, 70));
+					right_mouse_up(pos);*/
+
+					// send "Q"
+					send_key(0x51);
+					SLEEP(rand_int(35, 70));
+					send_key(0x51, false);
+
+					current_pos = pos;
+
+					// get next in the path
+					index = m_indexDropPath.front();
+					m_indexDropPath.pop_front();
+					step_counter++;
+
+					// restart timer
+					aim_timer.Start();
+				}
+				else
+				{
+					float t = std::clamp(aim_timer.Elapsed() / inventory_cleaner::delay, 0.f, 1.f);
+					int x_lerp = (int)slerp((float)current_pos.x, (float)pos.x, t);
+					int y_lerp = (int)slerp((float)current_pos.y, (float)pos.y, t);
+					SetCursorPos(x_lerp, y_lerp);
+				}
+
+				if (index == -1)
+					dropped = true;
+			}
+			else if (dropped && index != -1)
+			{
+				LOGDEBUG("MOVING ITEMS!");
+			}
+			else
+			{
+				if (m_isCtrlDown)
+				{
+					send_key(VK_CONTROL, false);
+					m_isCtrlDown = false;
+				}
+
+				cleaning = false;
+				dropped = false;
+			}
+
+			//LOGDEBUG("looting = true");
+			// do it here so we don't check toggle key press at same time for disabling
+			cleaning = true;
+		}
+		else
+		{
+			if (m_isCtrlDown)
+			{
+				send_key(VK_CONTROL, false);
+				m_isCtrlDown = false;
+			}
+
+			cleaning = false;
+			dropped = false;
+		}
+
+		if (index == -1)
+		{
+			if (m_isCtrlDown)
+			{
+				send_key(VK_CONTROL, false);
+				m_isCtrlDown = false;
+			}
+
+			cleaning = false;
+			dropped = false;
+		}
 	}
 	void CInventoryCleaner::OnImGuiRender(ImDrawList* draw)
 	{
@@ -134,9 +304,9 @@ namespace toadll
 			}
 		}
 
-		// for now it just randomizes the path
-		static auto rng = std::default_random_engine{};
-		std::ranges::shuffle(m_indexDropPath, rng);
+		//// for now it just randomizes the path
+		//static auto rng = std::default_random_engine{};
+		//std::ranges::shuffle(m_indexDropPath, rng);
 
 		m_indexDropPath.emplace_back(-1);
 	}
@@ -168,5 +338,19 @@ namespace toadll
 			return s.substr(pos + 1);
 		}
 		return "";
+	}
+
+	POINT CInventoryCleaner::get_middle_of_screen() const
+	{
+		RECT window_rect{};
+		if (!GetWindowRect(g_hWnd, &window_rect))
+		{
+			return { -1, -1 };
+		}
+
+		POINT pt{};
+		pt.x = window_rect.left + g_screen_width / 2;
+		pt.y = window_rect.top + g_screen_height / 2;
+		return pt;
 	}
 }
