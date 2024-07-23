cb3296bbc0fff341a8722199fdc116943d42a62f
himeji. <88136704+h1meji@users.noreply.github.com>
2024-07-23 10:39:20 +0200

    IC WIP | slot offset update func

--- ToadClient/src/Toad/Modules/inventory_cleaner.cpp
+++ ToadClient/src/Toad/Modules/inventory_cleaner.cpp
@@ -30,4 +30,41 @@ namespace toadll
 	void CInventoryCleaner::OnImGuiRender(ImDrawList* draw)
 	{
 	}
+
+	void CInventoryCleaner::UpdateSlotPosOffsets()
+	{
+		int index = 0;
+		ChestStealerSlotLocationInfo* info = nullptr;
+
+		if (inventory_cleaner::slot_info.res_x == g_screen_width && inventory_cleaner::slot_info.res_y == g_screen_height)
+		{
+			info = &inventory_cleaner::slot_info;
+			return;
+		}
+
+		if (inventory_cleaner::slot_info.res_x == -1 || inventory_cleaner::slot_info.res_y == -1)
+		{
+			info = &inventory_cleaner::slot_info;
+		}
+
+		if (!info)
+			return;
+
+		// hotbar
+		int hotbar_y = info->begin_y + (info->space_y * 3) + (info->space_y * 0.22) + 1;
+		for (int x = info->begin_x, slotx = 0; slotx < 9; x += info->space_x, slotx++)
+			m_slotPosOffset[index++] = { x, hotbar_y };
+
+		
+		// inv
+		for (int y = info->begin_y, sloty = 0; sloty < 3; y += info->space_y, sloty++)
+			for (int x = info->begin_x, slotx = 0; slotx < 9; x += info->space_x, slotx++)
+				m_slotPosOffset[index++] = { x, y };
+
+
+		// armor
+		int armor_y = info->begin_y - (info->space_y * 0.22) - 1 - info->space_y;
+		for (int y = armor_y, sloty = 0; sloty < 4; y -= info->space_y, sloty++)
+			m_slotPosOffset[index++] = { info->begin_x, y };
+	}
 }
